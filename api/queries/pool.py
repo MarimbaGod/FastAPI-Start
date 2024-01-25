7cf399e75a8cd7752e483647451ed76549b9ee99
Kyle <bossertkt@gmail.com>
2024-01-24 21:39:38 -0800

    werkrk
    added additional files as easy setup for postgres.

---
+++ api/queries/pool.py
@@ -0,0 +1,7 @@
+#If using a Postgres database, you can use the psycopg2 package to connect to it.
+
+# import os
+# from psycopg_pool import ConnectionPool
+# pool = ConnectionPool(conninfo=os.environ.get("DATABASE_URL"))
+
+#Uncomment these lines
